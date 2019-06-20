b7d2a58fbb82849fcb63e30472cd4bf17ba3d29f
hassanahmed95 <mr.hassanahmed95@gmail.com>
2019-06-20 16:29:32 +0500

    my first commit

---
+++ dl_face_detector.py
@@ -0,0 +1,6 @@
+# face detection with mtcnn on a photograph
+from matplotlib import pyplot
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+from mtcnn.mtcnn import MTCNN
+import cv2
---
+++ test.py
@@ -0,0 +1,12 @@
+import cv2
+import dlib
+import tensorflow
+import keras
+import sklearn
+
+
+print(dlib.__version__)
+print(cv2.__version__)
+print(tensorflow.__version__)
+print(keras.__version__)
+print(sklearn.__version__)

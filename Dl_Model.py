28e4fd8e307274ae277f20a2c0b8d84f26f7a35b
hassanahmed95 <mr.hassanahmed95@gmail.com>
2019-11-04 16:50:43 +0500

    the deep learning model has been upgraded and the test file as well

--- Dl_Model.py
+++ Dl_Model.py
@@ -6,18 +6,21 @@ import numpy as np
 from keras.callbacks import ModelCheckpoint
 from keras.preprocessing.image import ImageDataGenerator
 import os
-# he_uniform is the method for weights initalization, if we use relu as our activation function
+import matplotlib.pyplot as plt
+import pdb
+
 np.random.seed(1)
+
+# he_uniform is the method for weights initalization, if we use relu as our activation function
 
 
 def dl_model():
-
     nb_classes = 4
     # Initialising the CNN
     model = Sequential()
 
     # 1st Convolution
-    model.add(Conv2D(64, (3, 3), padding='same', input_shape=(48, 48, 1)))
+    model.add(Conv2D(64, (3, 3), padding='same', input_shape=(90, 90, 3)))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -47,9 +50,9 @@ def dl_model():
     # it is quite necessary to flat the inputs of conv layer before feeding to to dense layer
     # it make it uni dimension . . .
     model.add(Flatten())
-
     # Fully connected layer 1st layer
     model.add(Dense(256))
+    # batch normalization --> improving the speed of training using mean normalization
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Dropout(0.25))
@@ -69,22 +72,60 @@ def dl_model():
 
 def model_fitting():
     model = dl_model()
-
     batch_size = 128
+    epochs = 150
     BASE_PATH = os.path.dirname(__file__)
 
     checkpoint = ModelCheckpoint("model_weights.h5", monitor='val_acc', verbose=1, save_best_only=True, mode='max')
     callbacks_list = [checkpoint]
-
     datagen = ImageDataGenerator(rescale=1.0 / 255.0)
 
     train_it = datagen.flow_from_directory("{base_path}/Data_Testing/Training".format(base_path=BASE_PATH),
         class_mode='categorical', batch_size=batch_size, target_size=(90, 90))
-    test_it = datagen.flow_from_directory(  "{base_path}/Data_Testing/Testing".format(base_path=BASE_PATH),
-        class_mode='categorical', batch_size=16, target_size=(90, 90))
+
+    testing =  train_it.classes
+    print(testing)
+    exit()
+
+    test_it = datagen.flow_from_directory( "{base_path}/Data_Testing/Testing".format(base_path=BASE_PATH),
+        class_mode='categorical', batch_size=batch_size, target_size=(90, 90))
+    # for debugging
 
     history = model.fit_generator(train_it, steps_per_epoch=len(train_it),
-                                  validation_data=test_it, validation_steps=len(test_it), epochs=50, verbose=1,
+                                  validation_data=test_it, validation_steps=len(test_it), epochs=epochs, verbose=1,
                                   callbacks=callbacks_list)
+    # pdb.set_trace()
+    # saving the model in json format .
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+
+    # plotting the performance of the model in Matplotlib using history
+    return history
 
 
+def data_visualization(history):
+    plt.figure(figsize=(20, 10))
+    plt.subplot(1, 2, 1)
+    plt.suptitle('Optimizer : Adam', fontsize=10)
+    plt.ylabel('Loss', fontsize=16)
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.legend(loc='upper right')
+
+    plt.subplot(1, 2, 2)
+    plt.ylabel('Accuracy', fontsize=16)
+    plt.plot(history.history['acc'], label='Training Accuracy')
+    plt.plot(history.history['val_acc'], label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.savefig("Model_result.jpg")
+    plt.show()
+
+
+# main execution
+
+if __name__ == "__main__":
+    # model training
+    results = model_fitting()
+    # visualization of the results
+    data_visualization(results)
--- test_file.py
+++ test_file.py
@@ -1,90 +1,32 @@
-# make a prediction for a new image.
-#
-# from keras.preprocessing.image import load_img
-# from keras.preprocessing.image import img_to_array
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import load_model
-
-import numpy as np
+from keras.models import model_from_json
+import pdb
 from keras.preprocessing import image
-model = load_model('weights.best.hdf5')
-test_image = image.load_img('Updated_Data/test/Bore_faces/Bore1593.jpg', target_size=(90,90))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image,axis=0)
-result = model.predict(test_image)
-print(result)
-index_min = np.argmax(result[0])
-print(index_min)
-# for index, data in enumerate (result[0]):
-#     print(index, "", data)
+import os
+import numpy as np
 
 
-exit()
+def model_testing(model_json_file, model_weights_file):
+    # the reconstruction of the model from the json file
+    with open(model_json_file, 'r') as f:
+        ml_model = model_from_json(f.read())
+    ml_model.load_weights(model_weights_file)
+    return ml_model
 
 
+if __name__ == '__main__':
+    base_path = os.path.dirname(__file__)
+    json_file = "{base_path}/model.json".format(base_path=base_path)
+    weights_file = "{base_path}/model_weights.h5".format(base_path= base_path)
 
+    emotion_list = ["BORE", "INTERESTED", "NEUTRAL", "SURPRISE"]
+    model = model_testing(json_file, weights_file)
+    # pdb.set_trace()
 
+    test_image = image.load_img("Updated_Data/train/Bore_faces/Bore9.jpg", target_size=(90, 90))
+    test_image = image.img_to_array(test_image)
+    test_image = test_image/255
+    test_image = np.expand_dims(test_image, axis=0)
 
+    prediction = emotion_list[(int(np.argmax(model.predict(test_image))))]
+    print(prediction)
 
-
-
-
-
-
-
-
-test_dir = "/home/hassan/Hassaan_Home/My_Python_Projects/My_true_face_update/Data_Testing/Testing"
-
-test_datagen = ImageDataGenerator(rescale=1.0/255.0)
-
-test_generator = test_datagen.flow_from_directory(
-        test_dir,
-        target_size=(90,90),
-        shuffle = False,
-        class_mode='categorical',
-        batch_size=1)
-
-# print(test_generator)
-filenames = test_generator.filenames
-# print(filenames)
-nb_samples = len(filenames)
-
-model = load_model('weights.best.hdf5')
-
-predict = model.predict_generator(test_generator,steps = nb_samples)
-# print(len(predict))
-# print(type(predict))
-#
-print(predict)
-
-exit()
-
-# load and prepare the image
-def loadd_image(filename):
-    # load the image
-    img = load_img(filename)
-    # convert to array
-    img = img_to_array(img)
-    # reshape into a single sample with 3 channels
-    img = img.reshape(1, 90, 90, 3)
-    # center pixel data
-    # img = img.astype('float32')
-    # img = img - [123.68, 116.779, 103.939]
-    return img
-
-
-# load an image and predict the class
-def run_example():
-    # load the image
-    img = loadd_image('Updated_Data/test/Bore_faces/Bore111.jpg')
-    # load model
-    model = load_model('weights.best.hdf5')
-    # predict the class
-    result = model.predict(img)
-    # print(result)
-    for i in result[0]:
-        print(i)
-
-
-# entry point, run the example
-run_example()
